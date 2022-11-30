a398e30cd2691b89ae619ccd9751cb80ff32a5da
Hasaghi <tblonga6nd2020@gmail.com>
2022-11-30 12:14:24 +0700

    Update User

--- Server/src/Server/User.java
+++ Server/src/Server/User.java
@@ -1,53 +1,24 @@
-package Server;
+package AdminForm;
 
 import java.util.ArrayList;
 
 public class User {
-	private String password;
-	private String fullname;
-	private String address;
-	private String email;
+	private InforUser infor;
+	private ArrayList<String> timeLogin;
 	private ArrayList<String> friend;
 	private ArrayList<String> addFriendRequest;
 
-	public User(String password, String fullname, String address, String email) {
-		super();
-		this.password = password;
-		this.fullname = fullname;
-		this.address = address;
-		this.email = email;
+	public User() {
+		infor = new InforUser();
 	}
 
-	public String getPassword() {
-		return password;
+	public User(String username, String password, String fullname, String address, String DOB, String gender,
+			String email) {
+		infor = new InforUser(username, password, fullname, address, DOB, gender, email);
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getFullname() {
-		return fullname;
-	}
-
-	public void setFullname(String fullname) {
-		this.fullname = fullname;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
+	public ArrayList<String> getTimeLogin() {
+		return timeLogin;
 	}
 
 	public ArrayList<String> getFriend() {
@@ -57,11 +28,11 @@ public class User {
 	public void setFriend(ArrayList<String> friend) {
 		this.friend = friend;
 	}
-	
+
 	public void addFriend(String username) {
 		this.friend.add(username);
 	}
-	
+
 	public void deleteFriend(String username) {
 		this.friend.remove(this.friend.indexOf(username));
 	}
@@ -70,15 +41,19 @@ public class User {
 		return addFriendRequest;
 	}
 
-	public void setAddFriendRequest(ArrayList<String> addFriendRequest) {
-		this.addFriendRequest = addFriendRequest;
-	}
+//	public void setAddFriendRequest(ArrayList<String> addFriendRequest) {
+//		this.addFriendRequest = addFriendRequest;
+//	}
 
 	public void addAddFriendRequest(String username) {
 		this.addFriendRequest.add(username);
 	}
-	
+
 	public void deleteAddFriendRequest(String username) {
 		this.addFriendRequest.remove(this.addFriendRequest.indexOf(username));
+	}
+
+	public InforUser getInfor() {
+		return infor;
 	}
 }
