be2a0ebc8cc4d05232e7bf3bbad33284cd9f39c1
Phuoc Dinh <74655729+20127287@users.noreply.github.com>
2023-01-06 01:19:45 +0700

    Fix giao diện đăng ký tài khoản

--- Client/src/Client/SignUp.java
+++ Client/src/Client/SignUp.java
@@ -7,6 +7,8 @@ import java.awt.*;
 
 import javax.swing.border.EmptyBorder;
 import com.toedter.calendar.JDateChooser;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class SignUp extends JFrame {
 	/**
@@ -38,13 +40,20 @@ public class SignUp extends JFrame {
 	 * Attribute: SignUpStatus - status The status of Sign Up Request
 	 */
 	public static SignUpStatus status;
+	private JTextField fullnameTextField;
+	private JPanel contentPane;
+	private JTextField addressTextField;
+	private JTextField usernameTextField;
+	private JTextField emailTextField;
+	private JPasswordField passwordField;
+	private JPasswordField repasswordField;
 
 	/**
 	 * Default constructor
 	 */
 	public SignUp() {
 		addComponents();
-
+		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Đăng ký");
 		setResizable(false);
@@ -73,47 +82,54 @@ public class SignUp extends JFrame {
 
 	public void addComponents() {
 		// Content Pane
-		JPanel contentPane = new JPanel();
-		contentPane.setLayout(new GridLayout(11, 1, 0, 10));
+		setBounds(100, 100, 321, 489);
+		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
-
-		// Back to Sign In Button
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
 		JButton backButton = new JButton("Trở về");
 		backButton.addActionListener(e -> backButtonEventHandler());
 		backButton.setFocusPainted(false);
-
-		JPanel backPanel = new JPanel();
-		backPanel.setLayout(new BoxLayout(backPanel, BoxLayout.X_AXIS));
-		backPanel.add(backButton);
-
-		// Title Label
-		JLabel titleLabel = new JLabel("Đăng ký", JLabel.CENTER);
-		titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
-
-		// Fullname Panel
-		JTextField fullnameTextField = new JTextField(30);
-
-		JPanel fullnamePanel = new JPanel();
-		fullnamePanel.setLayout(new BoxLayout(fullnamePanel, BoxLayout.X_AXIS));
-		fullnamePanel.add(new JLabel("Họ và Tên"));
-		fullnamePanel.add(Box.createHorizontalStrut(56));
-		fullnamePanel.add(fullnameTextField);
-
-		// Gender Panel
+		backButton.setBounds(10, 10, 85, 21);
+		contentPane.add(backButton);
+		
+		JLabel lblngK = new JLabel("Đăng ký", SwingConstants.CENTER);
+		lblngK.setFont(new Font("Arial", Font.BOLD, 20));
+		lblngK.setBounds(65, 35, 180, 24);
+		contentPane.add(lblngK);
+		
+		JSeparator separator = new JSeparator();
+		separator.setForeground(Color.LIGHT_GRAY);
+		separator.setBounds(10, 69, 287, 1);
+		contentPane.add(separator);
+		
+		JLabel lblHVTn = new JLabel("Họ và tên");
+		lblHVTn.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblHVTn.setBounds(10, 89, 77, 13);
+		contentPane.add(lblHVTn);
+		
+		fullnameTextField = new JTextField();
+		fullnameTextField.setFont(new Font("Arial", Font.PLAIN, 12));
+		fullnameTextField.setColumns(10);
+		fullnameTextField.setBounds(128, 84, 169, 20);
+		contentPane.add(fullnameTextField);
+		
+		JLabel lblGiiTnh = new JLabel("Giới tính");
+		lblGiiTnh.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblGiiTnh.setBounds(10, 121, 77, 13);
+		contentPane.add(lblGiiTnh);
+		
 		JRadioButton radioBtn1 = new JRadioButton("Nam");
 		JRadioButton radioBtn2 = new JRadioButton("Nữ");
-		radioBtn1.setBounds(50, 60, 170, 30);
-		radioBtn2.setBounds(50, 100, 170, 30);
+		radioBtn1.setBounds(128, 114, 53, 22);
+		radioBtn2.setBounds(183, 114, 53, 22);
 		ButtonGroup bg = new ButtonGroup();
 		bg.add(radioBtn1);
 		bg.add(radioBtn2);
-
-		JPanel GenderPanel = new JPanel();
-		GenderPanel.setLayout(new BoxLayout(GenderPanel, BoxLayout.X_AXIS));
-		GenderPanel.add(new JLabel("Giới tính "));
-		GenderPanel.add(Box.createHorizontalStrut(60));
-		GenderPanel.add(radioBtn1);
-		GenderPanel.add(radioBtn2);
+		contentPane.add(radioBtn1);
+		contentPane.add(radioBtn2);
+		
 		String gender = new String();
 		if (radioBtn1.isSelected()) {
 			gender = "Nam";
@@ -122,91 +138,76 @@ public class SignUp extends JFrame {
 		} else {
 			gender = "";
 		}
-
-		JPanel CalendarPanel = new JPanel();
-
-		CalendarPanel.setLayout(new BoxLayout(CalendarPanel, BoxLayout.X_AXIS));
-
-		JLabel lblNewLabel = new JLabel("Ngay Sinh");
-		CalendarPanel.add(lblNewLabel);
-
-		Component horizontalStrut = Box.createHorizontalStrut(57);
-		CalendarPanel.add(horizontalStrut);
-
+		
+		JLabel lblNgySinh = new JLabel("Ngày sinh");
+		lblNgySinh.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblNgySinh.setBounds(10, 154, 77, 13);
+		contentPane.add(lblNgySinh);
+		
 		JDateChooser dateChooser = new JDateChooser();
-		CalendarPanel.add(dateChooser);
-
-		// Address Panel
-		JTextField addressTextField = new JTextField(15);
-
-		JPanel addressPanel = new JPanel();
-		addressPanel.setLayout(new BoxLayout(addressPanel, BoxLayout.X_AXIS));
-		addressPanel.add(new JLabel("Địa chi "));
-		addressPanel.add(Box.createHorizontalStrut(70));
-		addressPanel.add(addressTextField);
-
-		// Username Panel
-		JTextField usernameTextField = new JTextField(15);
-
-		JPanel usernamePanel = new JPanel();
-		usernamePanel.setLayout(new BoxLayout(usernamePanel, BoxLayout.X_AXIS));
-		usernamePanel.add(new JLabel("Tên tài khoản"));
-		usernamePanel.add(Box.createHorizontalStrut(40));
-		usernamePanel.add(usernameTextField);
-
-		// Email Panel
-		JTextField emailTextField = new JTextField(15);
-
-		JPanel emailPanel = new JPanel();
-		emailPanel.setLayout(new BoxLayout(emailPanel, BoxLayout.X_AXIS));
-		emailPanel.add(new JLabel("Email"));
-		emailPanel.add(Box.createHorizontalStrut(81));
-		emailPanel.add(emailTextField);
-
-		// Password Panel
-		JPasswordField passwordField = new JPasswordField(15);
-
-		JPanel passwordPanel = new JPanel();
-		passwordPanel.setLayout(new BoxLayout(passwordPanel, BoxLayout.X_AXIS));
-		passwordPanel.add(new JLabel("Mật khẩu"));
-		passwordPanel.add(Box.createHorizontalStrut(61));
-		passwordPanel.add(passwordField);
-		contentPane.add(passwordPanel);
-
-		// Re-Password Panel
-		JPasswordField repasswordField = new JPasswordField(15);
-
-		JPanel repasswordPanel = new JPanel();
-		repasswordPanel.setLayout(new BoxLayout(repasswordPanel, BoxLayout.X_AXIS));
-		repasswordPanel.add(new JLabel("Nhập lại mật khẩu"));
-		repasswordPanel.add(Box.createHorizontalStrut(20));
-		repasswordPanel.add(repasswordField);
-		contentPane.add(repasswordPanel);
-
-		// Sign Up Now Button
+		dateChooser.setBounds(128, 152, 172, 20);
+		contentPane.add(dateChooser);
+		
+		addressTextField = new JTextField();
+		addressTextField.setFont(new Font("Arial", Font.PLAIN, 12));
+		addressTextField.setColumns(10);
+		addressTextField.setBounds(128, 196, 169, 20);
+		contentPane.add(addressTextField);
+		
+		JLabel lblaCh = new JLabel("Địa chỉ");
+		lblaCh.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblaCh.setBounds(10, 201, 77, 13);
+		contentPane.add(lblaCh);
+		
+		JLabel lblTnTiKhon = new JLabel("Tên tài khoản");
+		lblTnTiKhon.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblTnTiKhon.setBounds(10, 245, 77, 13);
+		contentPane.add(lblTnTiKhon);
+		
+		usernameTextField = new JTextField();
+		usernameTextField.setFont(new Font("Arial", Font.PLAIN, 12));
+		usernameTextField.setColumns(10);
+		usernameTextField.setBounds(128, 240, 169, 20);
+		contentPane.add(usernameTextField);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblEmail.setBounds(10, 289, 77, 13);
+		contentPane.add(lblEmail);
+		
+		emailTextField = new JTextField();
+		emailTextField.setFont(new Font("Arial", Font.PLAIN, 12));
+		emailTextField.setColumns(10);
+		emailTextField.setBounds(128, 284, 169, 20);
+		contentPane.add(emailTextField);
+		
+		JLabel lblMtKhu = new JLabel("Mật khẩu");
+		lblMtKhu.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblMtKhu.setBounds(10, 333, 77, 13);
+		contentPane.add(lblMtKhu);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(128, 328, 169, 19);
+		contentPane.add(passwordField);
+		
+		repasswordField = new JPasswordField();
+		repasswordField.setBounds(128, 372, 169, 19);
+		contentPane.add(repasswordField);
+		
+		JLabel lblNhpLiMt = new JLabel("Nhập lại mật khẩu");
+		lblNhpLiMt.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblNhpLiMt.setBounds(10, 377, 102, 13);
+		contentPane.add(lblNhpLiMt);
+		
 		JButton signUpNowButton = new JButton("Đăng ký");
-
+		signUpNowButton.setBounds(10, 416, 287, 21);
 		signUpNowButton.addActionListener(e -> signUpNowButtonEventHandler(fullnameTextField.getText(),
-				SignUp.getSelectedButtonText(bg), dateChooser.getDate(), addressTextField.getText(),
-				usernameTextField.getText(), emailTextField.getText(), String.valueOf(passwordField.getPassword()),
-//                Array.toString(passwordField.getPassword()),
-				String.valueOf(repasswordField.getPassword())));
-		signUpNowButton.setFocusPainted(false);
-
-		// Add components to Content Pane and Settings
-		contentPane.add(backPanel);
-		contentPane.add(titleLabel);
-		contentPane.add(fullnamePanel);
-		contentPane.add(GenderPanel);
-		contentPane.add(CalendarPanel);
-		contentPane.add(addressPanel);
-		contentPane.add(usernamePanel);
-		contentPane.add(emailPanel);
-		contentPane.add(passwordPanel);
-		contentPane.add(repasswordPanel);
+		SignUp.getSelectedButtonText(bg), dateChooser.getDate(), addressTextField.getText(),
+		usernameTextField.getText(), emailTextField.getText(), String.valueOf(passwordField.getPassword()),
+//        Array.toString(passwordField.getPassword()),
+		String.valueOf(repasswordField.getPassword())));
+signUpNowButton.setFocusPainted(false);
 		contentPane.add(signUpNowButton);
-		setContentPane(contentPane);
-		pack();
 	}
 
 	/**
